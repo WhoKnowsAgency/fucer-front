9ffced291bab64471a27e7c513783dc3d493cdc9
Andy Damesón <andresdameson@gmail.com>
2021-02-10 21:31:13 -0300

    Maqueta página de certificado de curso

---
+++ pages/curso/_id/certificado.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="curso">
+    <div class="container">
+      <ABreadcrumb>
+        <ABreadcrumbItem>
+          <NuxtLink to="/campus">Campus</NuxtLink>
+        </ABreadcrumbItem>
+        <ABreadcrumbItem>
+          <NuxtLink :to="curso.url">Curso</NuxtLink>
+        </ABreadcrumbItem>
+        <ABreadcrumbItem>Certificado</ABreadcrumbItem>
+      </ABreadcrumb>
+      <div>
+        <h1>Certificado</h1>
+        <p class="intro">Visualizá o descargá el certificado de cursada.</p>
+      </div>
+
+      <template v-if="curso.certificado">
+        Acá va a estar mi hermoso certificado.
+      </template>
+      <template v-else>
+        <AEmpty>
+          <div slot="description">
+            <h3>Aún no se emitió tu certificado de cursada</h3>
+            <p>Recibirás una notificación cuando puedas acceder.</p>
+          </div>
+        </AEmpty>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    curso() {
+      return this.$store.state.cursos.byId[this.$route.params.id];
+    },
+  },
+  head() {
+    return {
+      title: "Certificado - " + this.curso.nombre,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.curso .container {
+  margin-top: 55px;
+  margin-bottom: 200px;
+}
+
+h1 {
+  font-weight: bold;
+  font-size: 30px;
+  line-height: 38px;
+  margin-top: 16px;
+  margin-bottom: 20px;
+  color: @daybreak-blue-10;
+}
+
+h2 {
+  color: @daybreak-blue-10;
+}
+
+.intro {
+  max-width: 380px;
+  margin-bottom: 60px;
+}
+
+.card {
+  max-width: 384px;
+  background: white;
+  h3 {
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 24px;
+    color: @daybreak-blue-10;
+  }
+  .btn {
+    margin-top: 17px;
+  }
+}
+
+@media (max-width: 460px) {
+  .ant-col /deep/ {
+    width: 100%;
+  }
+}
+</style>
